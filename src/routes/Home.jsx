f32d7e9b266a33bad01cf6803c17b99a783e62e1
Daniel <danieldossantosdacosta@gmail.com>
2022-11-27 23:44:00 -0300

    configuration for get from api

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import blogFetch from "../axios/config";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [posts, setPosts] = useState([]);
 
-export default Home
+  const getPosts = async () => {
+    try {
+      const response = await blogFetch.get("/posts");
+
+      const data = response.data;
+      console.log(data);
+
+      setPosts(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+  return (
+    <div>
+      <h1>Últimos posts</h1>
+    </div>
+  );
+};
+
+export default Home;
