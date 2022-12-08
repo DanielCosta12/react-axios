b236397a305784d967759cbf7e3f6c892bd7f1b8
Daniel <danieldossantosdacosta@gmail.com>
2022-12-07 22:58:22 -0300

    add react spinners. (I need try to add on component for this spinners)

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,9 +5,10 @@ import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 import "./Home.css";
-
+import ClipLoader from "react-spinners/ClipLoader";
 const Home = () => {
-  const [posts, setPosts] = useState([]);
+  const [posts, setPosts, loading, setLoading] = useState([]);
+  let [color, setColor] = useState("#ffffff");
 
   const getPosts = async () => {
     try {
@@ -29,7 +30,13 @@ const Home = () => {
     <div className="home">
       <h1>Últimos posts</h1>
       {posts.length === 0 ? (
-        <p>Carregando...</p>
+        <ClipLoader
+          color={color}
+          loading={loading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
       ) : (
         posts.map((post) => (
           <div className="post" key={post.id}>
--- src/routes/Post.jsx
+++ src/routes/Post.jsx
@@ -3,13 +3,13 @@ import blogFetch from "../axios/config";
 import { useState, useEffect } from "react";
 
 import { useParams } from "react-router-dom";
-
+import ClipLoader from "react-spinners/ClipLoader";
 import "./Post.css";
 
 const Post = () => {
   const { id } = useParams();
-  const [post, setPost] = useState([]);
-
+  const [post, setPost, loading, setLoading] = useState([]);
+  let [color, setColor] = useState("#ffffff");
   const getPost = async () => {
     try {
       const response = await blogFetch.get(`/posts/${id}`);
@@ -29,7 +29,13 @@ const Post = () => {
   return (
     <div className="post-container">
       {!post.title ? (
-        <p>Carregando...</p>
+        <ClipLoader
+          color={color}
+          loading={loading}
+          size={150}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
       ) : (
         <div className="post">
           <h2>{post.title}</h2>
