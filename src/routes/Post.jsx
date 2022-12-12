ca6bdef5ba28a701634ddb60e13f87e78ae851be
Daniel <danieldossantosdacosta@gmail.com>
2022-12-12 17:35:13 -0300

    add loading component in post

--- src/routes/Post.jsx
+++ src/routes/Post.jsx
@@ -3,13 +3,12 @@ import blogFetch from "../axios/config";
 import { useState, useEffect } from "react";
 
 import { useParams } from "react-router-dom";
-import ClipLoader from "react-spinners/ClipLoader";
+import Loading from "../components/Loading";
 import "./Post.css";
 
 const Post = () => {
   const { id } = useParams();
-  const [post, setPost, loading, setLoading] = useState([]);
-  let [color, setColor] = useState("#ffffff");
+  const [post, setPost] = useState([]);
   const getPost = async () => {
     try {
       const response = await blogFetch.get(`/posts/${id}`);
@@ -29,13 +28,7 @@ const Post = () => {
   return (
     <div className="post-container">
       {!post.title ? (
-        <ClipLoader
-          color={color}
-          loading={loading}
-          size={150}
-          aria-label="Loading Spinner"
-          data-testid="loader"
-        />
+        <Loading />
       ) : (
         <div className="post">
           <h2>{post.title}</h2>
